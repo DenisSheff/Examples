36fea5aaa8b8825f43f91244683ff90b1ca2e9a2
Denis Shevchenko <denis_sheff@icloud.com>
2022-09-29 21:16:44 +0900

    Example006 Heaviest dumbbell is found

---
+++ Example006_DumbBell/Program.cs
@@ -0,0 +1,15 @@
+﻿int firstDumbBell = 1;
+int secondDumbBell = 2;
+int thirdDumbBell = 6;
+int fourthDumbBell = 8;
+int fifthDumbBell = 4;
+
+int maxNumber = firstDumbBell;
+
+if (firstDumbBell > maxNumber) maxNumber = firstDumbBell;
+if (secondDumbBell > maxNumber) maxNumber = secondDumbBell;
+if (thirdDumbBell > maxNumber) maxNumber = thirdDumbBell;
+if (fourthDumbBell > maxNumber) maxNumber = fourthDumbBell;
+if (fifthDumbBell > maxNumber) maxNumber = fifthDumbBell;
+
+Console.WriteLine("Max number is " + maxNumber + "kg");
