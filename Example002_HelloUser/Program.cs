aa87dfe826381aa14ae69f8f43ff49d83ae8736e
Denis Shevchenko <denis_sheff@icloud.com>
2022-09-29 15:43:44 +0900

    Example 002 Hello, username

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine(); //Запрашивает информацию у пользователя непосредственно в самом терминале.
+Console.WriteLine("Hello, " + username + "!");
