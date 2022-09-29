80e841882a89ac4b39d5513b9a79892524cbc4e4
Denis Shevchenko <denis_sheff@icloud.com>
2022-09-29 20:45:26 +0900

    Example005 Favorite user

---
+++ Example005_FavoriteUser/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+
+string userName = Console.ReadLine();
+
+if (userName == "John")
+{
+	Console.WriteLine("Hey, it is John! Finally!");
+}
+else
+{
+	Console.WriteLine("Well, hi, " + userName);
+}
+
